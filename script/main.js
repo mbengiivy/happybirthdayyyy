83950d6eab7bc21365909b101d0725d6567e01bb
Mbengiivy <mbengiivy@gmail.com>
2025-12-01 15:03:04 +0300

    new color

--- script/main.js
+++ script/main.js
@@ -224,7 +224,7 @@ const animationTimeline = () => {
         }, {
             scale: 1,
             rotationY: 0,
-            color: "#ff69b4",
+            color: "#8a69ffff",
             ease: Expo.easeOut,
         },
         0.1,
